d858cf627329d91e9fe1d1634a1c41edb28625f4
restel15 <70467225+restel15@users.noreply.github.com>
2023-12-10 22:04:01 +0300

    Delete question_answer.py

--- question_answer.py
+++
@@ -1,5 +0,0 @@
-from openpyxl import load_workbook
-
-async def question_answer(name) -> list:
-    file = load_workbook("data/tests/" + name + ".xlsx").active
-    return tuple(file.iter_cols(values_only=True))
